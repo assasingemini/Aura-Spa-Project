f5e57d8717fb0a292ea24b85424810f9aba4d66e
Besna <152285973+assasingemini@users.noreply.github.com>
2026-03-16 00:15:08 +0700

    fix: suppress Edge Runtime warnings in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "res.cloudinary.com",
-                pathname: "/dvlpkot5z/**",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dvlpkot5z/**",
+      },
+    ],
+  },
+  // Suppress Edge Runtime warnings from jose/next-auth during build
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.ignoreWarnings = [
+        { module: /node_modules\/jose/ },
+        { module: /node_modules\/@prisma/ },
+        { module: /node_modules\/bcryptjs/ },
+      ];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
